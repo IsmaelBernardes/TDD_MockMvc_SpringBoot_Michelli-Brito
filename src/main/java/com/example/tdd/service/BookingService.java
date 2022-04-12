d8877b91264fb3defed8ee2dad93d77442b9e1d1
ismaelbernardes <ismael12.bernardes@gmail.com>
2022-04-11 21:16:26 -0400

    pasta model criada com arquivo java 'BookingModel.java', e pasta service com 'BookingService.java' e arquivo de teste 'BookingServiceTest.java'

---
+++ src/main/java/com/example/tdd/service/BookingService.java
@@ -0,0 +1,13 @@
+package com.example.tdd.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookingService {
+
+	public int daysCalculatorWithDatabase(String name) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
